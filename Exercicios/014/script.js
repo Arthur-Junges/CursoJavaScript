5c42cc534f6cb6498ab4d547f786b5633da71573
Arthur-Junges <Arthur.jungesH@gmail.com>
2026-01-22 09:46:13 -0300

    vetores

---
+++ Exercicios/014/script.js
@@ -0,0 +1,9 @@
+let num = [5,8,2,9,3]
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+console.log(`O vetor organizado né ${num.sort()}`)
+console.log(`O vetor com um numero adicionado no final ${num.push(1)}`)
+let pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
+console.log(num)
