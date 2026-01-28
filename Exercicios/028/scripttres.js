ef623ae1202dd1bc1f11365f2612186ff234a0f6
Arthur-Junges <Arthur.jungesH@gmail.com>
2026-01-28 16:25:39 -0300

    Callback

---
+++ Exercicios/028/scripttres.js
@@ -0,0 +1,26 @@
+/* FUNÇÃO PASSADA COM ARGUMENTO
+function fazerPizza(tipodoqueijo, tipodorecheio){
+    const pizza = `pizza de ${tipodoqueijo} com ${tipodorecheio}`
+    return pizza
+}
+
+const pizzadocliente = fazerPizza('Gorgonzola', 'Bacon')
+console.log(pizzadocliente)
+*/
+
+function exibir(num){
+    console.log('A operação resultou em : ' + num)
+}
+
+function soma (a,b, callback){
+    var op = a + b
+    callback(op)
+}
+
+function multi(a,b, callback){
+    var op = a * b
+    callback(op)
+}
+
+soma(2,2, exibir)
+multi(2,4, exibir)
